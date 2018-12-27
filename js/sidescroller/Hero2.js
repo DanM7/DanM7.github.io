ea7895df684aabe98df4340d63c012cbde2dcc9d
Dan Maguire <DanMaguire7@gmail.com>
2018-12-27 14:34:39 -0500

    remove alerts

--- js/sidescroller/Hero2.js
+++ js/sidescroller/Hero2.js
@@ -25,7 +25,8 @@ const HERO_DEFAULT_SIZE_OFFSET_Y = 13;
 class Hero2 extends Phaser.Sprite {
     constructor(game, x, y, spriteName) {
         super(game, x + 40, y, spriteName);
-        alert("Hero2 constructor start")
+        
+        //alert("Hero2 constructor start")
         this.anchor.set(0.5, 0.5);
 
         // physics properties
@@ -65,7 +66,7 @@ class Hero2 extends Phaser.Sprite {
 
         // Make starting animation separate funcion:
         this.animations.play('animationHeroIdle');
-        alert("Hero2 constructor end")
+        //alert("Hero2 constructor end")
     }
 
     //#region Animations
